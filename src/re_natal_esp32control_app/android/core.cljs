357c0d67575d0afd2a502558da2f61681ed9afda
asuki <asukiaaa@gmail.com>
2017-11-23 18:37:06 +0900

    Separate common root and initialization to config

--- src/re_natal_esp32control_app/android/core.cljs
+++ src/re_natal_esp32control_app/android/core.cljs
@@ -3,24 +3,14 @@
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
             [re-natal-esp32control-app.events]
             [re-natal-esp32control-app.subs]
-            [re-natal-esp32control-app.devices.ble :as ble]
-            [re-natal-esp32control-app.devices.magnetometer :as mag]
-            [re-natal-esp32control-app.views.common :as v.common]
-            [re-natal-esp32control-app.views.ble-control :as v.ble-control]
-            [re-natal-esp32control-app.views.top :as v.top]))
+            [re-natal-esp32control-app.config :as config]))
 
 (def ReactNative (js/require "react-native"))
 (def app-registry (.-AppRegistry ReactNative))
 
 (defn app-root []
-  (let [page (subscribe [:get-page])]
-    (fn []
-      (case @page
-        :ble-control [v.ble-control/ble-control-page]
-        [v.top/top-page]))))
+  (config/app-root))
 
 (defn init []
-  (dispatch-sync [:initialize-db])
-  (ble/init)
-  (mag/init)
+  (config/init-once)
   (.registerComponent app-registry "reNatalEsp32ControlApp" #(r/reactify-component app-root)))
---
+++ src/re_natal_esp32control_app/root.cljs
@@ -0,0 +1,24 @@
+(ns re-natal-esp32control-app.root
+  (:require [reagent.core :as r]
+            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+            [re-natal-esp32control-app.events]
+            [re-natal-esp32control-app.subs]
+            [re-natal-esp32control-app.devices.ble :as ble]
+            [re-natal-esp32control-app.devices.magnetometer :as mag]
+            [re-natal-esp32control-app.views.common :as v.common]
+            [re-natal-esp32control-app.views.ble-control :as v.ble-control]
+            [re-natal-esp32control-app.views.top :as v.top]))
+
+(defn app-root []
+  (let [page (subscribe [:get-page])]
+    (r/create-class
+     {:component-will-mount
+      (fn []
+        (prn "app-root initted")
+        (ble/init)
+        (mag/init))
+      :reagent-render
+      (fn []
+        (case @page
+          :ble-control [v.ble-control/ble-control-page]
+          [v.top/top-page]))})))
